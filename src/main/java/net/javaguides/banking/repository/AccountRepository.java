3c4f15115f7206a784bdfeb84377dba22cc01383
Anuththara <asbdehipitiya@gmail.com>
2024-11-05 23:00:23 +0530

    AccountRepository file changes

--- src/main/java/net/javaguides/banking/repository/AccountRepository.java
+++ src/main/java/net/javaguides/banking/repository/AccountRepository.java
@@ -1,4 +1,7 @@
 package net.javaguides.banking.repository;
 
-public interface AccountRepository {
+import net.javaguides.banking.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
