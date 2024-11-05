65549422cc9cc336392073ab95a06ba2e7893516
Anuththara <asbdehipitiya@gmail.com>
2024-11-05 21:50:52 +0530

    AccountDto AccountMapper AccountService AccountServiceImpl Above files are created

---
+++ src/main/java/net/javaguides/banking/dto/AccountDto.java
@@ -0,0 +1,10 @@
+package net.javaguides.banking.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
---
+++ src/main/java/net/javaguides/banking/entity/mapper/AccountMapper.java
@@ -0,0 +1,4 @@
+package net.javaguides.banking.entity.mapper;
+
+public class AccountMapper {
+}
